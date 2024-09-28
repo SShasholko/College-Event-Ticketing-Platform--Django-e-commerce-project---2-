71d652123edc214b9a4ee2366399cb09d3412cd5
Svitlana Shasholko <shasholko.sveta@gmail.com>
2024-09-28 19:22:23 +0100

    Add Event Images

--- event_ticketing/events/admin.py
+++ event_ticketing/events/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import Event
 
 # Register your models here.
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_time', 'venue', 'ticket_price')
+    # Display the image in the admin (optional)
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        return obj.image and f'<img src="{obj.image.url}" width="100" />'
+    image_preview.allow_tags = True
+    image_preview.short_description = 'Image Preview'
+
+admin.site.register(Event, EventAdmin)
--- event_ticketing/events/models.py
+++ event_ticketing/events/models.py
@@ -10,6 +10,7 @@ class Event(models.Model):
     ticket_price = models.DecimalField(max_digits=10, decimal_places=2)
     total_tickets = models.IntegerField()
     remaining_tickets = models.IntegerField()
+    image = models.ImageField(upload_to='event_images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
