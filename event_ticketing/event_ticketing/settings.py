9eac263103d8ee6b40bd54480964a06777dbd67f
Svitlana Shasholko <shasholko.sveta@gmail.com>
2024-11-16 13:31:32 +0000

    Install dj_database_url

--- event_ticketing/event_ticketing/settings.py
+++ event_ticketing/event_ticketing/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import environ
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +122,7 @@ WSGI_APPLICATION = 'event_ticketing.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 # Password validation
